9679c19c7d53f48dd962df273452aa0c4a27d110
Adrian Quintana <adrianquintana@gmail.com>
2022-05-10 12:28:30 +0100

    make explicit the package is public in npm

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -3,7 +3,6 @@ set -e -u -o pipefail
 
 version=$(cat package.json | jq -r '.version')
 publish_opts=$(echo $version | grep -q beta && echo "--tag beta" || true)
-yarn publish $publish_opts --new-version $version build/
-
+yarn publish $publish_opts --new-version $version build/ --access public
 git tag v$version -f
 git push --tags
